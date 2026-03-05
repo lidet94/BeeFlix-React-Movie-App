0e623ca0f3efd7e3b2e2819e01e3faa141ea9d4f
Bana <b.teklay@live.co.uk>
2026-03-05 15:30:34 +0000

    Updated the Home/My List buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 import { SearchBar } from './SearchBar'
 /*logo + search bar + maybe MyList button*/
-export const NavBar = () => {
+export const NavBar = ({ setPage }) => {
 
     
     
   return (
  <nav>
-    <h4>BEEFLIX</h4>
+    <h4 onClick={() => setPage("home")}>BEEFLIX</h4>
+
+      <button onClick={() => setPage("home")}>
+        Home
+      </button>
+
+      <button onClick={() => setPage("mylist")}>
+        My List
+      </button>
  </nav>      
   )
 }
