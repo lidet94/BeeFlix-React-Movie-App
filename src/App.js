8f2d934d764cb97d625bb9fa97be2b9b3fdab168
Bana <b.teklay@live.co.uk>
2026-03-05 15:26:20 +0000

    Added a page state and MyList logic to switch between Home and MyList views

--- src/App.js
+++ src/App.js
@@ -4,25 +4,59 @@ import { Hero } from './components/Hero';
 import { NavBar } from './components/NavBar';
 import { Row } from './components/Row';
 import { SearchBar } from './components/SearchBar';
-import { MyListContext } from './context/MyListContext';
 import Home from './pages/Home'
+import movies from '../src/data/movies.json'
+import MyList from './pages/MyList';
 
 function App() {
 const [search, setSearch] = useState('');
+const [myList, setMyList] = useState([]);
+const [page, setPage] = useState("home");
 
     const handleSearch = (term) =>{
         setSearch(term)
       }
 
+function addToMyList(movie){
+setMyList((prev)=>[...prev, movie])
+}
+
+function removeMyList(id){
+setMyList((prev)=> prev.filter(item => item.id !== id))
+}
+
+function isInMyList (id){
+  return myList.some(item => item.id === id)
+}
 
   return (
    <>
-   <NavBar />
+   <NavBar  setPage={setPage}/>
+   
+    {page === "home" && (
+      <>
    <SearchBar onSearch={handleSearch} />
    <Hero />
-   <Home />
-   <Row/>
-   <MyListContext search={search}/>
+   <Home 
+   search={search}
+   addToMyList={addToMyList} 
+        removeMyList={removeMyList} 
+        isInMyList={isInMyList}/>
+          </>)}
+  
+   {page === "mylist" && (
+   <MyList 
+   movies={myList}
+        addToMyList={addToMyList} 
+        removeMyList={removeMyList} 
+        isInMyList={isInMyList}
+   />)}
+  
+   <Row movies={movies}
+   addToMyList={addToMyList} 
+        removeMyList={removeMyList} 
+        isInMyList={isInMyList}/>
+   
    </>
   );
 }
