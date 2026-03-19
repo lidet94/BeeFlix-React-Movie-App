b8ceafa14eab2eff1a444f85e057e5b21ece1edd
Bana <b.teklay@live.co.uk>
2026-03-19 19:29:49 +0000

    Added props and conditional rendering for search results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,46 @@
 /*hero + rows*/
 import { organiseByCategory } from "../utils/organiseByCategory"
-import  movies from '../data/movies.json'
+import movies from '../data/movies.json'
 import { Row } from "../components/Row"
 
 
-export default function Home() {
+export default function Home({ search, addToMyList, removeMyList, isInMyList }) {
 
   const grouped = organiseByCategory(movies)
 
+  const filteredMovies = movies.filter(movie =>
+    movie.movie_title.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div className="home">
-      {Object.keys(grouped).map(category=>(
-        <Row 
-        key={category}
-        title={category}
-        movies={grouped[category]}
-        
+      {search ? (
+
+        <Row
+          title="Search Results"
+          movies={filteredMovies}
+          addToMyList={addToMyList}
+          removeMyList={removeMyList}
+          isInMyList={isInMyList}
         />
-        
-      ))}
+
+      ) : (
+
+        Object.keys(grouped).map(category => (
+          <Row
+            key={category}
+            title={category}
+            movies={grouped[category]}
+            addToMyList={addToMyList}
+            removeMyList={removeMyList}
+            isInMyList={isInMyList}
+          />
+
+        )))}
+
+
+
+
     </div>
   )
 }
