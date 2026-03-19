a65d922f8efce6a33ffaedcc08d7ba91bcd63460
Bana <b.teklay@live.co.uk>
2026-03-19 21:39:05 +0000

    Added modal functionality to More Info button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
 import '../styles/hero.css'
 import movies from '../data/movies.json'
+import { Modal } from './Modal'
 
-export const Hero = () => {
+
+export const Hero = ({ addToMyList, removeMyList, isInMyList}) => {
 
   const trendingMovies = movies.filter(movie => movie.isTrending === true)
+
   const [trendingMovie] = useState(trendingMovies[Math.floor(Math.random() * trendingMovies.length)]
   )
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false)
 
   return (
     <div className='hero-section' style={{ backgroundImage: `url(${trendingMovie.poster_image})` }}>
@@ -16,7 +24,22 @@ export const Hero = () => {
 
       <div className='hero-buttons'>
         <button className='play-btn'>Play</button>
-        <button className='info-btn'>More Info</button>
+        <button className='info-btn' onClick={openModal}>More Info</button>
+
+        {isOpen && (
+          <Modal
+            isOpen={isOpen}
+            movie={trendingMovie}
+            onClose={closeModal}
+            addToMyList={addToMyList}
+            removeMyList={removeMyList}
+            isInMyList={isInMyList}
+          />
+        )}
+
+
+
+
       </div>
 
     </div>
