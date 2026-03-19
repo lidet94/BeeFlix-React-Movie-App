d98b707e82c01176bdf5a80ec4ffb58dc5c21e59
Bana <b.teklay@live.co.uk>
2026-03-19 19:44:04 +0000

    Removed unused imports, added dark theme, and moved search handling to Navbar

--- src/App.js
+++ src/App.js
@@ -1,63 +1,59 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Hero } from './components/Hero';
 import { NavBar } from './components/NavBar';
-import { Row } from './components/Row';
-import { SearchBar } from './components/SearchBar';
 import Home from './pages/Home'
-import movies from '../src/data/movies.json'
 import MyList from './pages/MyList';
 
 function App() {
-const [search, setSearch] = useState('');
-const [myList, setMyList] = useState([]);
-const [page, setPage] = useState("home");
 
-    const handleSearch = (term) =>{
-        setSearch(term)
-      }
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }, []);
 
-function addToMyList(movie){
-setMyList((prev)=>[...prev, movie])
-}
+  const [search, setSearch] = useState('');
+  const [myList, setMyList] = useState([]);
+  const [page, setPage] = useState("home");
 
-function removeMyList(id){
-setMyList((prev)=> prev.filter(item => item.id !== id))
-}
+  const handleSearch = (term) => {
+    setSearch(term)
+  }
 
-function isInMyList (id){
-  return myList.some(item => item.id === id)
-}
+  function addToMyList(movie) {
+    setMyList((prev) => [...prev, movie])
+  }
+
+  function removeMyList(id) {
+    setMyList((prev) => prev.filter(item => item.id !== id))
+  }
+
+  function isInMyList(id) {
+    return myList.some(item => item.id === id)
+  }
 
   return (
-   <>
-   <NavBar  setPage={setPage}/>
-   
-    {page === "home" && (
-      <>
-   <SearchBar onSearch={handleSearch} />
-   <Hero />
-   <Home 
-   search={search}
-   addToMyList={addToMyList} 
-        removeMyList={removeMyList} 
-        isInMyList={isInMyList}/>
-          </>)}
-  
-   {page === "mylist" && (
-   <MyList 
-   movies={myList}
-        addToMyList={addToMyList} 
-        removeMyList={removeMyList} 
-        isInMyList={isInMyList}
-   />)}
-  
-   <Row movies={movies}
-   addToMyList={addToMyList} 
-        removeMyList={removeMyList} 
-        isInMyList={isInMyList}/>
-   
-   </>
+    <>
+      <NavBar setPage={setPage} onSearch={handleSearch} />
+
+      {page === "home" && (
+        <>
+
+          <Hero />
+          <Home
+            search={search}
+            addToMyList={addToMyList}
+            removeMyList={removeMyList}
+            isInMyList={isInMyList} />
+        </>)}
+
+      {page === "mylist" && (
+        <MyList
+          myList={myList}
+          addToMyList={addToMyList}
+          removeMyList={removeMyList}
+          isInMyList={isInMyList}
+        />)}
+    </>
   );
 }
 
